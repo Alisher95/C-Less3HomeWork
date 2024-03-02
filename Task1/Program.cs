2814ed6fa56a9a3eb30e8cb7614945331ba076f6
Myltykbayev Alisher <mr.aliw1995@gmail.com>
2024-03-02 15:09:01 +0600

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int[] array = new int[10];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 101);
+    Console.Write(array[i] + " ");
+}
+Console.Write("    =>  ");
+foreach (int item in array)
+{
+    if (20 <= item && item <= 90)
+    {
+        count++;
+    }
+}
+Console.Write(count);
