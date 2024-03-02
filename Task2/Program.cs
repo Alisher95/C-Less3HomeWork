dee460bdb6434448cbb3fdd18af96302b112f0ca
Myltykbayev Alisher <mr.aliw1995@gmail.com>
2024-03-02 16:17:47 +0600

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Задайте массив на 10 целых чисел. 
+//Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 11);
+    Console.Write(array[i] + " ");
+}
+Console.Write(" =>  ");
+foreach (int item in array)
+{
+    if (item % 2 == 0)
+        count++;
+}
+Console.Write(count);
